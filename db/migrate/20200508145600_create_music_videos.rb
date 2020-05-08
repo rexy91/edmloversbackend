53a5c296cc1e1ec3c43c4c99ee570df34d30181b
Rex Ye <rxye1991@gmail.com>
2020-05-08 10:56:27 -0400

    generatedMusicVideoModel

---
+++ db/migrate/20200508145600_create_music_videos.rb
@@ -0,0 +1,12 @@
+class CreateMusicVideos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :music_videos do |t|
+      t.string :name
+      t.string :artist
+      t.string :duration
+      t.belongs_to :genre, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
