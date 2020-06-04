b1525c0edec44ba1c5b39ceeb93a9bac6bda5a0a
Rex Ye <rxye1991@gmail.com>
2020-06-04 17:42:28 -0400

    addedroutelogin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,25 @@
 class UsersController < ApplicationController
 
     def index
-        byebug
+        
         @users = User.all
         render json: @users
     end
 
     def create
-        byebug
+        @user = User.create(user_params)
+        if @user
+            byebug
+        end
+    end
+
+    def login
+
     end
 
     private
     def user_params
             params.permit(:username, :password, )
     end
-
 end
+
