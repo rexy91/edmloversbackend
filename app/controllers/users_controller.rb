d4e18bf741c4a7c8a12ac38668e4e38f0d0e4a50
Rex Ye <rxye1991@gmail.com>
2020-06-06 20:45:43 -0400

    addedauthuncationcodeinApplicationController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.create(username:params[:username], password:params[:password])
-        byebug
+
         if @user
             byebug
         end
